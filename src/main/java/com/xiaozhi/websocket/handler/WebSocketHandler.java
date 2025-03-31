56191364da4df08dc426491beda731f5ec93ebc3
JoeyZhou <zzq980308zzq@gmail.com>
2025-03-31 16:22:39 +0800

    fix:修复音频文件生成问题

--- src/main/java/com/xiaozhi/websocket/handler/WebSocketHandler.java
+++ src/main/java/com/xiaozhi/websocket/handler/WebSocketHandler.java
@@ -117,7 +117,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
         if (deviceResult.isEmpty()) {
             SysDevice codeResult = deviceService.generateCode(device);
             String audioFilePath;
-            if (StringUtils.hasText(codeResult.getAudioPath())) {
+            if (!StringUtils.hasText(codeResult.getAudioPath())) {
                 audioFilePath = textToSpeechService.textToSpeech("请到设备管理页面添加设备，输入验证码" + codeResult.getCode());
                 codeResult.setDeviceId(device.getDeviceId());
                 codeResult.setSessionId(sessionId);
