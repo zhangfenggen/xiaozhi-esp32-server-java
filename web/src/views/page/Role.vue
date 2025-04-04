505723bc341121299aa563157c679205a3a6f9d2
JoeyZhou <zzq980308zzq@gmail.com>
2025-04-04 23:20:03 +0800

    fix:修复微软测试语音传参错误问题

--- web/src/views/page/Role.vue
+++ web/src/views/page/Role.vue
@@ -976,7 +976,7 @@ export default {
           voiceName: values.voiceName,
           provider: this.selectedProvider,
           message: this.testText,
-          ttsId: this.selectedttsId // 始终传递TTS配置ID
+          ttsId: this.selectedProvider === 'edge' ? -1 : this.selectedttsId
         };
 
         axios
